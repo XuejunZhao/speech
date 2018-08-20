9a7d5c669b8fa8da7a48a51ddb8a717e3c45505e
zhaoxuejun_i <zhaoxuejun_i@didichuxing.com>
2018-08-20 22:30:26 +0800

    model

---
+++ python/generate_silence_wav.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Ren Zhang @ ryanzjlib dot gmail dot com
+
+import os
+import sys
+import numpy as np
+from scipy.io import wavfile
+
+def main():
+    np.random.seed(0)
+    #mkdirs mkdir
+    PATH = os.path.dirname(os.path.abspath(__file__))
+    print PATH
+    Pre_PATH = os.path.split(PATH)[0]
+    out_path = os.path.join(Pre_PATH,'input/train/audio/silence/')
+
+    if not os.path.exists(out_path):
+        os.mkdir(out_path)
+    reduce_PATH = os.path.join(out_path,'*')
+    os.system("rm -rf *.wav")
+    # os.system("rm -rf '%s'") %(reduce_PATH)
+    #     os.mkdirs('../input/train/audio/silence')
+    # os.system('rm -rf ../input/train/audio/silence/*')
+
+    path = os.path.join(Pre_PATH,'input/train/audio/_background_noise_/')
+
+    # path = '../input/train/audio/_background_noise_/'
+    # out_path = '../input/train/audio/silence/'
+
+    files = [fp for fp in os.listdir(path) if fp.endswith('.wav')]
+
+    for filename in files:
+        print(filename)
+        sample_rate, samples = wavfile.read(path + filename)
+        print(len(samples))
+        for i in range(400):
+            out_name = "segment_{}_".format(i) + filename
+            data = (samples[i * 200: i * 200 + sample_rate] * max(0, 2 * (np.random.random() - 0.25))).astype('int16')
+            if data.max() == 0:
+                print(out_name)
+            wavfile.write(out_path + out_name, sample_rate, data)
+
+    for i in range(500):
+        d = np.zeros(16000)
+        loc = np.random.randint(0, 16000, 4600)
+        d[loc[::2]] = -1
+        d[loc[1::2]] = 1
+        wavfile.write(out_path + 'new_synthesised_{}.wav'.format(i), 16000, d)
+
+if __name__ == '__main__':
+    main()
